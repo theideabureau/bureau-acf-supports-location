6004dcc2a23a9e48495cb47dbf529dfe60aef15b
Ben Everard <hello@beneverard.co.uk>
2015-03-24 11:23:48 +0000

    Initial commit

---
+++ acf-supports-location.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+	Plugin Name: ACF Supports Location
+	Plugin URI: https://github.com/theideabureau/acf-supports-location
+	Version: 0.1
+	Author: The Idea Bureau
+	Description: 
+	Text Domain: acf-supports-location
+	License: GPLv3
+*/
+
+
+add_filter('acf/location/rule_types', function($choices) {
+
+	$choices['Supports']['supports'] = 'Post Type Support';
+
+	return $choices;
+
+});
+
+add_filter('acf/location/rule_values/supports', function($choices) {
+
+	$choices = array_merge($choices, array(
+		'title' => 'Title',
+		'editor' => 'Editor',
+		'author' => 'Author',
+		'thumbnail' => 'Thumbnail',
+		'excerpt' => 'Excerpt',
+		'trackbacks' => 'Trackbacks',
+		'custom-fields' => 'Custom Fields',
+		'comments' => 'Comments',
+		'revisions' => 'Revisions',
+		'page-attributes' => 'Page Attributes',
+		'post-formats' => 'Post Formats'
+	));
+
+	return $choices;
+
+});
+
+add_filter('acf/location/rule_match/supports', function($match, $rule, $options) {
+
+	$supported = post_type_supports(get_post_type(), $rule['value']);
+	$required = $rule['operator'] === '==';
+
+	return $supported === $required;
+
+}, 10, 3);
